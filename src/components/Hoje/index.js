a01a4a0930ed70229fdabc84f0720e1ae39f8061
bruno torres <brunotorres0717@gmail.com>
2023-04-08 12:32:12 -0300

    Adicionando arquivos do projeto

---
+++ src/components/Hoje/index.js
@@ -0,0 +1,35 @@
+import classNames from "classnames";
+import styles from "./Hoje.module.css";
+import { BsCloudsFill, BsCloudRainFill, BsCloudLightningRainFill, BsSunFill, BsCloudSunFill, BsFillCloudMoonFill, BsFillCloudSnowFill, BsCloudFog2Fill, BsCloudHailFill, BsFillMoonStarsFill } from "react-icons/Bs"
+
+export default function Hoje({ city, currently, description, temp, time, wind_speedy, date, condition_slug, sunset, sunrise }) {
+   return (
+      <div className={classNames({
+         [styles.card]:true,
+         [styles.card__nublado]: condition_slug == "cloudly_day"
+      })}>
+         <h1 className={styles.cidade}>{city}</h1>
+         <span>{currently} - {date} - Atualizado: {time}</span>
+         <div className={styles.icon__container}>
+            {condition_slug == "rain"  && <BsCloudRainFill size={40} />}   {/* Chovendo */}
+            {condition_slug == "cloud" && <BsCloudsFill size={40} />}   {/* Nublado */}   
+            {condition_slug == "storm" && <BsCloudLightningRainFill size={40} />}   {/*Tempestade*/}   
+            {condition_slug == "clear_day" && <BsSunFill size={40} />}  {/* Dia Limpo */}   
+            {condition_slug == "clear_night" && <BsFillMoonStarsFill size={40} />}   {/* Noite Limpa*/}   
+            {condition_slug == "cloudly_day" && <BsCloudSunFill size={40} />} {/*Dia Nublado */}   
+            {condition_slug == "cloudly_night" && <BsFillCloudMoonFill size={40} />}  {/* Noite Nublada*/}   
+            {condition_slug == "fog" && <BsCloudFog2Fill size={40} />}   {/* Neblina */}   
+            {condition_slug == "snow" && <BsFillCloudSnowFill size={40} />}  {/* Nevando */}   
+            {condition_slug == "hail" && <BsCloudHailFill size={40} />}  {/* Granizo */}   
+            <p>{temp}°C </p>
+         </div>
+         <div className={styles.tempo}>
+            <p>{description}</p>
+            <p>Vento: {wind_speedy}</p>
+            <p>Nascer do Sol: {sunrise}</p>
+            <p>Pôr do Sol: {sunset}</p>
+         </div>
+
+      </div>
+   )
+}
---
+++ src/components/Previsao/index.js
@@ -0,0 +1,33 @@
+import classNames from "classnames";
+import styles from "./Previsao.module.css";
+import { BsCloudsFill, BsCloudRainFill, BsCloudLightningRainFill, BsSunFill, BsCloudSunFill, BsFillCloudMoonFill, BsFillCloudSnowFill, BsCloudFog2Fill, BsCloudHailFill, BsFillMoonStarsFill } from "react-icons/Bs"
+
+export default function PrevisaoCard({ min, max, description, rain_probability, weekday, condition, dia}) {
+   return (
+      <div className={classNames({
+         [styles.dia]:true,
+         [styles.dia__nublado]: condition == "cloudly_day" || condition == "cloud",
+         [styles.dia__sol]: condition == "clear_day",
+         [styles.dia__chuva]: condition == "rain",
+
+      })}>
+         <h3 className={styles.dia__semana}>{weekday} - {dia}</h3>
+         {condition == "rain" && <BsCloudRainFill size={40} />}   {/* Chovendo */}
+         {condition == "cloud" && <BsCloudsFill size={40} />}   {/* Nublado */}
+         {condition == "storm" && <BsCloudLightningRainFill size={40} />}   {/*Tempestade*/}
+         {condition == "clear_day" && <BsSunFill size={40} />}  {/* Dia Limpo */}
+         {condition == "clear_night" && <BsFillMoonStarsFill size={40} />}   {/* Noite Limpa*/}
+         {condition == "cloudly_day" && <BsCloudSunFill size={40} />} {/*Dia Nublado */}
+         {condition == "cloudly_night" && <BsFillCloudMoonFill size={40} />}  {/* Noite Nublada*/}
+         {condition == "fog" && <BsCloudFog2Fill size={40} />}   {/* Neblina */}
+         {condition == "snow" && <BsFillCloudSnowFill size={40} />}  {/* Nevando */}
+         {condition == "hail" && <BsCloudHailFill size={40} />}  {/* Granizo */}
+         <div className={styles.temperaturas}>
+            <p className={styles.text}>Max: {max}°C</p>
+            <p className={styles.text}>Min: {min}°C</p>
+         </div>
+         <p className={styles.text}>{description}</p>
+         <p className={styles.text}>Probalidade de chuva: {rain_probability}%</p>
+      </div>
+   )
+}
